6767ddc91f47f4c6060adcec1c23e9ee7b7cbed4
mbahraoui <mohamedbahraoui20@gmail.com>
2023-03-29 02:30:32 +0000

    creating first model

--- base/views.py
+++ base/views.py
@@ -1,10 +1,22 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request,'home.html')
+rooms=[
+    {'id':1,'name':'python'},
+    {'id':2,'name':'javascript'},
+    {'id':3,'name':'c++'},
+]
 
-def room(request):
-    return render(request,'room.html')
+def home(request):
+    context={'rooms':rooms}
+    return render(request,'base/home.html',context)
+
+def room(request,id):
+    room=None
+    for i in rooms:
+        if i['id']==int(id):
+            room=i
+    print(room)
+    context={'room':room}
+    return render(request,'base/room.html',context)
